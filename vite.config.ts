2c4d0d354639c6e2be8d478b44c16de71507b9b2
posatsky <posatsky@gmail.com>
2026-08-10 18:14:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(() => {
   return {
     base:'/WortFan'/,
     build: {
-      outDir: 'docs',
+      outDir: 'dist',
       assetsDir: 'assets',
       sourcemap: false,
     },
